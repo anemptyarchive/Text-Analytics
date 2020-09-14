3c67ddbe8ce047ca1c9d7411ef4d1c9f07416948
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-09-14 10:07:28 +0900

    update ch5.2

--- Code/Code_ch5.R
+++ Code/Code_ch5.R
@@ -8,6 +8,7 @@
 library(RMeCab)
 library(dplyr)
 
+
 # 抽出する品詞を指定
 PoS <- c("名詞")
 
@@ -16,6 +17,7 @@ dir_path <- "text_data_cp932/kobushi"
 
 # 形態素解析
 res_mecab <- docDF(dir_path, type = 1, pos = PoS)
+
 
 # 文書単語行列を作成
 doc_term_mat <- res_mecab %>% 
@@ -36,7 +38,42 @@ doc_term_mat <- res_mecab %>%
 
 # 5.2 特異値分解 ---------------------------------------------------------------
 
+# 特異値分解
+res_svd <- svd(doc_term_mat)
+
+# 特異値を取得
+d <- res_svd[["d"]]
+
+# Uを取得
+U <- res_svd[["u"]]
+
+# Vを取得
+V <- res_svd[["v"]]
+
+# 検証
+dplyr::near(doc_term_mat, U %*% diag(d) %*% t(V))
+
+
+# 行列を作成
+mat <- matrix(1:15, nrow = 3, ncol = 5)
+mat
+
+# 特異値分解
+res <- svd(mat)
+d <- res[["d"]]
+U <- res[["u"]]
+V <- res[["v"]]
+U; diag(d); V
+
+# 検証
+U %*% diag(d) %*% t(V)
+
 
 # 5.3 主成分分析 ---------------------------------------------------------------
+
+
+
+# 5.4 対応分析 ----------------------------------------------------------------
+
 
 
